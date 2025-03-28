d902b72a9de1c9062618d1c7a0caef6495dcd73c
Ho Yin Chau <hchau630@berkeley.edu>
2025-03-27 20:38:32 -0400

    update processs_rossi.py and update plot/weights_space.toml, but will likely revert this and previous commit since rossi data is not absolute probability

--- paper/process_rossi.py
+++ paper/process_rossi.py
@@ -11,7 +11,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=Path)
     parser.add_argument("--bootstrap", "-b", action="store_true")
-    parser.add_argument("--file-type", "-t", choices=["csv", "pkl"], default="csv")
+    parser.add_argument("--file-type", "-t", choices=["np", "pd"], default="np")
     parser.add_argument("--out", "-o", type=Path)
     args = parser.parse_args()
 
@@ -21,25 +21,31 @@ def main():
     is_positive = data["rbins"] >= 0
     data = {k: v[is_positive] for k, v in data.items()}
 
+    outs = {}
     for filename, name in [("EE", "rEX"), ("EI", "rIN")]:
         out = [data["rbins"], data[name].mean(axis=1)]
         if args.bootstrap:
             out += stats.bootstrap((data[name].T,), np.mean).confidence_interval
-        out = np.stack(out, axis=1)
-        if args.out:
+        outs[filename] = np.stack(out, axis=1)
+    
+    if args.out:
+        if args.file_type == "csv":
             args.out.mkdir(exist_ok=True)
-            if args.file_type == "csv":
-                header = f"command: {' '.join(sys.argv)}"
-                np.savetxt(
-                    args.out / f"{filename}.csv", out, delimiter=",", header=header
-                )
-            else:
-                breaks = (out[:-1, 0] + out[1:, 0]) / 2
-                breaks = np.r_[0, breaks, breaks[-1] + breaks[0]]
-                out = pd.DataFrame(out, columns=["distance", "mean", "low", "high"])
-                out["distance"] = pd.IntervalIndex.from_breaks(breaks)
-                out.attrs["command"] = " ".join(sys.argv)
-                out.to_pickle(args.out / f"{filename}.pkl")
+            header = f"command: {' '.join(sys.argv)}"
+            np.savetxt(args.out / f"{filename}.csv", out, delimiter=",", header=header)
+        else:
+            breaks = (data["rbins"][:-1] + data["rbins"][1:]) / 2
+            breaks = np.r_[0, breaks, breaks[-1] + breaks[0]]
+            df = []
+            for k, v in outs.items():
+                v = pd.DataFrame(v, columns=["distance", "density", "low", "high"])
+                v["distance"] = pd.IntervalIndex.from_breaks(breaks)
+                v["postsynaptic_cell_type"] = k[0]
+                v["presynaptic_cell_type"] = k[1]
+                df.append(v)
+            df = pd.concat(df).reset_index(drop=True)
+            df.attrs["command"] = " ".join(sys.argv)
+            df.to_pickle(args.out)
 
 
 if __name__ == "__main__":
